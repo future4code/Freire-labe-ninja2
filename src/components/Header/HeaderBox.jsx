97b5acb37c658aa541e40a4e7fdad19458a950cb
Diego Guimarães <diego.guimaraes.si@gmail.com>
2022-06-27 19:26:52 -0300

    substituindo h1 por h3

--- src/components/Header/HeaderBox.jsx
+++ src/components/Header/HeaderBox.jsx
@@ -13,8 +13,8 @@ export const HeaderBox = ({quantItensCart, setQuantItensCart}) => {
             <img src="https://cdn-icons.flaticon.com/png/512/1126/premium/1126816.png?token=exp=1656358537~hmac=c22c0306d95a7190fb69f1b6b6f65454" alt=""/>
 
             <nav>
-                <h1>Como funciona?</h1>
-                <h1>Serviços</h1>
+                <h3>Como funciona?</h3>
+                <h3>Serviços</h3>
                 <HiOutlineShoppingCart/>
             </nav>
         </header>
