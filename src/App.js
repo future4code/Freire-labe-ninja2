d7daa3fe238b38ff0dfa6ed6315da015f5dee172
Diego Guimarães <diego.guimaraes.si@gmail.com>
2022-06-30 14:54:25 -0300

    criando filtros

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
-import logo from "./logo.svg";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 /*Components*/
 import { HeaderBox } from "./components/Header/HeaderBox";
-import JobList from "./components/List/JobsList";
+import { JobsList } from "./components/List/JobsList";
+import { SearchInput } from "./components/Search/SearchInput";
+import { JobFilter } from "./components/Filter/JobFilter";
+
 
 function App() {
   const [quantItensCart, setQuantItensCart] = useState(0);
 
+  /*Armazena todos os produtos da loja*/
+  const [jobsList, setJobsList] = useState([]);
+  const [selectedBrand, setSelectedBrand] = useState("MENOR");
+  const [minimo, setMinimo] = useState(0);
+  const [maximo, setMaximo] = useState(1000);
+  const [query, setQuery] = useState("");
+
+  useEffect = () => {
+    axios.get("https://labeninjas.herokuapp.com/jobs", {
+      headers: { Authorization: "2d88d553-bd93-447d-871c-2849315c7ded" },
+    })
+      .then((response) => {
+        this.setJobsList({ jobsList: response.data.jobs });
+        console.log(response.data.jobs);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
   return (
     <div className="App">
       <HeaderBox
         quantItensCart={quantItensCart}
         setQuantItensCart={setQuantItensCart}
       />
-      <JobList />
+      <div className="Main">
+        <SearchInput
+          query={query}
+          setQuery={setQuery}
+
+        />
+
+        <JobFilter
+          setJobsList={setJobsList}
+          selectedBrand={selectedBrand}
+          setSelectedBrand={setSelectedBrand}
+          minimo={minimo}
+          setMinimo={setMinimo}
+          maximo={maximo}
+          setMaximo={setMaximo}
+          query={query}
+          setQuery={setQuery}
+        />
+        <JobsList
+          jobsList={jobsList}
+          selectedBrand={selectedBrand}
+          minimo={minimo}
+          maximo={maximo}
+          query={query}
+        />
+      </div>
     </div>
   );
 }
--- src/components/List/JobsList.jsx
+++ src/components/List/JobsList.jsx
@@ -1,36 +1,43 @@
 import React from "react";
-import axios from "axios";
 
-class JobsList extends React.Component {
-  state = {
-    lista: [],
-  };
+import { CartItem } from "../Cart/CartItem";
 
-  componentDidMount = () => {
-    this.getJobs();
-  };
+export const JobsList = ({ jobsList, selectedBrand, minimo, maximo, query }) => {
 
-  getJobs = () => {
-    const request = axios.get("https://labeninjas.herokuapp.com/jobs", {
-      headers: { Authorization: "2d88d553-bd93-447d-871c-2849315c7ded" },
-    })
-      .then((response) => {
-        this.setState({ lista: response.data.jobs });
-        console.log(this.state.lista);
-      })
-      .catch((error) => {
-        alert(error.message);
-      });
-  };
-
-  render() {
-    const listJobs = this.state.lista.map((job) => {
-      return <div>{job.title}</div>;
-    });
-
-    console.log(this.state.lista);
-    return <div>{listJobs}</div>;
-  }
+  return (
+    <div>
+      {jobsList
+        .filter(job => {
+          return job.name.toLowerCase().includes(query.toLowerCase())
+        })
+        .filter(job => {
+          return minimo === "" || job.price >= minimo
+        })
+        .filter(job => {
+          return maximo === "" || job.price <= maximo
+        })
+        .sort((currentProduct, nextProduct) => {
+          console.log(selectedBrand)
+          switch (selectedBrand) {
+            case "MENOR":
+              return currentProduct.price - nextProduct.price
+            case "MAIOR":
+              return nextProduct.price - currentProduct.price
+            case "CRESCENTE":
+              return nextProduct.name.localeCompare(currentProduct.name)
+            case "DECRESCENTE":
+              return -1 * nextProduct.name.localeCompare(currentProduct.name)
+            default:
+              return currentProduct.name - nextProduct.name
+          }
+        })
+        .map(job => {
+          return <CartItem
+            key={job.id}
+            job={job}
+          />
+        })
+      }
+    </div>
+  )
 }
-
-export default JobsList;
